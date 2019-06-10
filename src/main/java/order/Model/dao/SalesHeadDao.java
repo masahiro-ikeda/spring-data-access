aaa5a4d3632f8a97f799429776fe39e5ba85a16f
masahiro.ikeda <masahiro.ikeda@MyComputer>
2019-06-10 12:25:32 +0900

    modify

--- src/main/java/order/Model/dao/SalesHeadDao.java
+++ src/main/java/order/Model/dao/SalesHeadDao.java
@@ -1,107 +1,113 @@
 package order.Model.dao;
 
-import order.Model.dto.SalesHeadDto;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Component;
 
-import javax.sql.DataSource;
-import java.sql.*;
+import order.Model.dto.SalesHeadDto;
 
 @Component
 public class SalesHeadDao {
 
-    @Autowired
-    DataSource dataSource;
+	@Autowired
+	DataSource dataSource;
 
-    /**
-     * 売上ヘッダデータを保存
-     *
-     * @param head
-     */
-    public void insertHead(SalesHeadDto head) {
+	/**
+	 * 売上ヘッダデータを保存
+	 *
+	 * @param head
+	 */
+	public void insertHead(SalesHeadDto head) {
 
-        Connection con = null;
-        PreparedStatement ps = null;
+		Connection con = null;
+		PreparedStatement ps = null;
 
-        try {
-            con = DataSourceUtils.getConnection( dataSource );
+		try {
+			con = DataSourceUtils.getConnection(dataSource);
 
-            StringBuilder builder = new StringBuilder();
-            builder.append( "INSERT INTO sales_head ( " );
-            builder.append( "    sales_no,            " );
-            builder.append( "    customer_name,       " );
-            builder.append( "    created_at           " );
-            builder.append( ") VALUES (               " );
-            builder.append( "    ?,                   " );
-            builder.append( "    ?,                   " );
-            builder.append( "    ?                    " );
-            builder.append( ")                        " );
+			StringBuilder builder = new StringBuilder();
+			builder.append("INSERT INTO sales_head ( ");
+			builder.append("    sales_no,            ");
+			builder.append("    customer_name,       ");
+			builder.append("    created_at           ");
+			builder.append(") VALUES (               ");
+			builder.append("    ?,                   ");
+			builder.append("    ?,                   ");
+			builder.append("    ?                    ");
+			builder.append(")                        ");
 
-            ps = con.prepareStatement( builder.toString() );
-            //ps.setInt( 1, head.getSalesNo() );
-            //ps.setString( 2, head.getCustomerName() );
-            ps.setTimestamp( 3, new Timestamp( System.currentTimeMillis() ) );
+			ps = con.prepareStatement(builder.toString());
+			//ps.setInt( 1, head.getSalesNo() );
+			//ps.setString( 2, head.getCustomerName() );
+			ps.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
 
-            ps.executeUpdate();
+			ps.executeUpdate();
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (ps != null) {
-                    ps.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (ps != null) {
+					ps.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-    /**
-     * 次の売上Noを発行する
-     *
-     * @return 次の売上No
-     */
-    public int getNextSalesNo() {
+	/**
+	 * 次の売上Noを発行する
+	 *
+	 * @return 次の売上No
+	 */
+	public int getNextSalesNo() {
 
-        Connection con = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
 
-        int nextNo = 0;
-        try {
-            con = DataSourceUtils.getConnection( dataSource );
+		int nextNo = 0;
+		try {
+			con = DataSourceUtils.getConnection(dataSource);
 
-            StringBuilder builder = new StringBuilder();
-            builder.append( "SELECT                    " );
-            builder.append( "  MAX(sales_no) AS max_no " );
-            builder.append( "FROM                      " );
-            builder.append( "  sales_head              " );
+			StringBuilder builder = new StringBuilder();
+			builder.append("SELECT                    ");
+			builder.append("  MAX(sales_no) AS max_no ");
+			builder.append("FROM                      ");
+			builder.append("  sales_head              ");
 
-            ps = con.prepareStatement( builder.toString() );
-            rs = ps.executeQuery();
+			ps = con.prepareStatement(builder.toString());
+			rs = ps.executeQuery();
 
-            if (rs.next()) {
-                nextNo = rs.getInt( "max_no" );
-                nextNo += 1;
-            }
+			if (rs.next()) {
+				nextNo = rs.getInt("max_no");
+				nextNo += 1;
+			}
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (ps != null) {
-                    ps.close();
-                }
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return nextNo;
-    }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (ps != null) {
+					ps.close();
+				}
+				if (rs != null) {
+					rs.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return nextNo;
+	}
 
 }
--- src/main/java/order/OrderService.java
+++ src/main/java/order/OrderService.java
@@ -1,20 +1,12 @@
 package order;
 
-import order.Model.dao.SalesHeadDao;
-import order.Model.dto.SalesHeadDto;
-import order.Model.dao.SalesLineDao;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
+import order.Model.dto.SalesHeadDto;
 
 @Service
 public class OrderService {
 
-    @Autowired
-    private SalesHeadDao hDao;
-    private SalesLineDao lDao;
-
-    @Transactional
     public void executeOrder(OrderForm form) {
 
         SalesHeadDto hDto = new SalesHeadDto();
